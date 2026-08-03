c4faa667545d2586781b4a92b8bfb38c7e72fcc3
Ashish Jadhav <ashishaniljadhav0409@gmail.com>
2026-08-03 11:03:58 +0530

    comparison of datatypes

---
+++ 05_datatypes_comparison.js
@@ -0,0 +1,22 @@
+console.log(2 > 1)
+console.log(2 < 1)
+console.log(2 >= 1)
+console.log(2 <= 1)
+console.log(2 == 1)
+console.log(2 != 1)
+console.log(2 == "2")
+console.log(2 === "2")
+console.log(2 !== "2")  
+
+//avoid this converssion of string to number and number to string, because it can lead to unexpected results. Always use explicit conversion methods like Number() or String() for clarity and to avoid bugs.
+console.log(null > 0)
+console.log(null >= 0)
+console.log(null == 0)
+console.log(null < 0)
+console.log(null <= 0)  
+
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log(undefined == 0)
+console.log(undefined >= 0)
+console.log(undefined <= 0) 
