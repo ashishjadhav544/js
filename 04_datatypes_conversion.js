20dcd8972d66d667ad5578c1662d095085be1e82
Ashish Jadhav <ashishaniljadhav0409@gmail.com>
2026-08-03 10:50:45 +0530

    conversion of string to number confuse conversion and prefix and postfix operators

--- 04_datatypes_conversion.js
+++ 04_datatypes_conversion.js
@@ -32,3 +32,48 @@ let someNumber = 33;
 let someNumberAsString = String(someNumber);
 console.log(typeof someNumberAsString);
 console.log(someNumberAsString);
+
+
+
+
+
+let number = 100;
+
+let stringNumber = String(number);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+let str1 = "ashish"
+let str2 = "jadhav"
+
+let str3 = str1+str2;
+
+console.log(str3);
+
+console.log("1" + 2);
+
+console.log(1 + "2");
+
+console.log("1" + 2 + 2);
+
+console.log(1 + 2 + "2");
+
+
+
+//prefix and postfix increment and decrement operators
+
+let num = 5;
+
+console.log(num++);
+console.log(num);
+
+
+let x = 5;
+
+console.log(++x);
+console.log(x);
+
+
+
