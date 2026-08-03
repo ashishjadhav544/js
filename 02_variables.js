422ea9d3660cf485152145c48293ab859200e263
Ashish Jadhav <ashishaniljadhav0409@gmail.com>
2026-08-03 07:48:21 +0530

    variables-const,let,var

---
+++ 02_variables.js
@@ -0,0 +1,47 @@
+const accountId = 123456;
+let accountEmail = "ashish@gmail.com";
+var accountName = "ashish";
+accountCity = "Bangalore";
+
+
+//  accountId = 12; //type error because accountId is a constant variable and cannot be reassigned. 
+accountEmail = "anil@gmail.com";
+accountName = "anil";
+accountCity = "Delhi";
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountName, accountCity]);
+
+
+
+
+
+
+
+//example of chatgpt 
+// var a = 10;
+// let b = 20;
+// const c = 30;
+
+// a = 15;
+// b = 25;
+
+// // c = 35; // Uncomment and observe the error
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// if (true) {
+//     var x = 100;
+//     let y = 200;
+//     const z = 300;
+
+//     console.log(x);
+//     console.log(y);
+//     console.log(z);
+// }
+
+// console.log(x);
+//  //console.log(y); // Uncomment and observe the error
+//  console.log(z); // Uncomment and observe the error
