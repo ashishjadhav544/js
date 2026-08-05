fcb62e8105547b9b92578cd5a012b7115495534f
Ashish Jadhav <ashishaniljadhav0409@gmail.com>
2026-08-05 07:29:06 +0530

    learn function in js

---
+++ 13_functions.js
@@ -0,0 +1,59 @@
+//function is a block of code that can be called and executed when needed
+
+// function myname(){
+//   console.log("a");
+//   console.log("s");
+//   console.log("h");
+//   console.log("i");
+//   console.log("s");
+//   console.log("h")
+// }
+
+// myname()
+
+
+// function addtwonummber(num1, num2){
+//   console.log(num1 + num2);
+// }
+
+// addtwonummber(1,2);
+
+
+// function addtwonumber(num1, num2){
+//   let result = num1 + num2
+//   return result;
+// }
+// let result = addtwonumber(1,3);
+// console.log(result);
+
+
+
+// function newuser(username){
+//   console.log(`${username} just logged in`);
+// }
+// newuser("ashish");
+
+
+
+// function newuser1(username){
+//   if(username === undefined){
+//     console.log("please enter a name")
+//     return
+//   }
+//   return `${username} just logged in`
+// }
+// console.log(newuser1());
+
+
+// function newuser1(username = "ashish"){
+//   // if(username === undefined){
+//   if(!username){
+//     console.log("please enter a name")
+//     return
+//   }
+//   return `${username} just logged in`
+// }
+// console.log(newuser1("anil"));//overrides
+
+
+
