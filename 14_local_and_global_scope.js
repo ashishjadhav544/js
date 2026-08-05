72e6f149aabbfeb5787c17e9bbc8f930a1775bcf
Ashish Jadhav <ashishaniljadhav0409@gmail.com>
2026-08-05 08:13:07 +0530

    local and global scope

---
+++ 14_local_and_global_scope.js
@@ -0,0 +1,13 @@
+const b = 25;
+var c =35;
+
+if(true){
+let a =10;
+const b =20;
+var c =30;
+console.log("Local Scope",a);
+}
+
+// console.log(a);
+console.log("global Scope",b);
+console.log(c);
