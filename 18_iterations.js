06c12f55d0448c956861d20516be9f0a929df25f
Ashish Jadhav <ashishaniljadhav0409@gmail.com>
2026-08-06 08:41:10 +0530

    for loop in js

---
+++ 18_iterations.js
@@ -0,0 +1,79 @@
+//for 
+// for (initialization; condition; increment/decrement) {
+//     // code
+// }
+
+// for(let i =0; i<=10; i++){
+//   const element = i;
+//   console.log(element);
+// }
+
+
+// for(let i =0; i<=10; i++){
+//   const element = i;
+//   if(element == 4){
+//     console.log("my birthday")
+//   }
+//   console.log(element);
+// }
+
+
+
+// for(i=0;i<=10;i++){
+//   console.log(i);
+//   for(j=0;j<=10;j++){
+//     console.log(j);
+//   }
+// }
+
+
+
+// for(i=1;i<=10;i++){
+//   for(j=1;j<=10;j++){
+//     console.log(i + '*' + j  + '=' + i*j);
+//   }
+// }
+
+
+
+//for loop with arrays
+
+// const super_heros = ["captain", "ironman", "hulk", "thor"]
+
+// for(let i=0;i<super_heros.length;i++){
+//   const element = super_heros[i];
+//   console.log(element);
+// }
+
+
+
+//break
+
+// Stops the loop immediately.
+
+// for(i=0;i<=10;i++){
+//   if(i==5){
+//     console.log("stops at 5");
+//     break;
+//   }
+//   console.log(`my number is ${i}`);
+// }
+
+
+
+
+// continue
+
+// Skips only the current iteration.
+
+// for(i=0;i<=10;i++){
+//   if(i==5){
+//     console.log("stops at 5");
+//     continue;
+//   }
+//   console.log(`my number is ${i}`);
+// }
+
+
+
+
